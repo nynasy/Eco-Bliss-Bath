a20127070ca618e8463f5bc313dfb95ab8865ffb
maimouna <nynasy@gmail.com>
2024-11-04 00:36:56 +0100

    tests avis

--- cypress/e2e/1-api/reviews.cy.js
+++ cypress/e2e/1-api/reviews.cy.js
@@ -45,5 +45,194 @@ describe("Api avis", () => {
     })   
 
 
+    it("Ajouter un avis sans titre", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false,
+        body: {
+          title: null,
+          comment: 'top',
+          rating: 5,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+
+
+    it("Ajouter un avis sans note", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: 'top',
+          comment: 'fonctionnelle',
+          rating: null,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+
+    it("Ajouter un avis sans commentaires", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: 'top',
+          comment: null,
+          rating: 5,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+
+    it("Ajouter un avis avec une note superieur à 5", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: 'top',
+          comment: 'fonctionnelle',
+          rating: 6,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+    it("Ajouter un avis avec une note inferieur à 1", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: 'top',
+          comment: 'fonctionnelle',
+          rating: 0,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+    it("Ajouter un avis avec une chaîne non numerique dans la note", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: 'top',
+          comment: 'fonctionnelle',
+          rating: "ddddddddd",
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+
+    it("Ajouter un avis avec une chaîne de plus de 255 caractères", () => {
+      cy.getLocalStorage('token').then(token => {
+        console.log('token', token)
+     
+        cy.request({
+        method: 'POST',
+        url : '/reviews',
+        failOnStatusCode: false ,
+        body: {
+          title: "a".repeat(256),
+          comment: 'fonctionnelle',
+          rating: 5,
+        },
+
+        headers : {
+            Authorization: 'Bearer ' + token
+        }
+      })
+      .then((orders) => {       
+          expect(orders.status).to.eq(400);   
+
+        });
+      })
+    })   
+
+
+
+
+
+    
+
+
+
+
         
   });
